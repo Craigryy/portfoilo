5c52b8504d8ba8eaf75289e945f9f4e026b9217b
Craigryy <harriajames97@gmail.com>
2023-08-19 15:59:00 +0100

    added an initial README.md

--- react-frontend/src/components/About.jsx
+++ react-frontend/src/components/About.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import myphoto from './images/myphoto.jpg';
+import '../App.css'; 
+
 
 function About() {
   return (
     <div>
       <section id="about-me">
         <div className="my-photo">
-            <img src="./images/myphoto.jpg" alt="myPhoto" id="my-photo"></img>
+            <img src={myphoto} alt="My Photo" id="my-photo" />
             <h4>Harrison James</h4>
-            <p>10 Articles</p>
+            <br/>
+            <button className='btn btn-danger'>say Hi </button>
         </div>
         <div id="about-me-text">
-          <h3>
+          {/* <h3>
             <span>About Me</span>
-          </h3>
+          </h3> */}
           <p>
-            I am <span>Software Engineer</span>. I have always been in awe of web pages that pay attention to aesthetics and also creativity. I have then derived pleasure in creating web products that are responsive and user-friendly.
+            "I am a passionate software engineer with a deep fascination for the intricate logic behind various concepts.
+             I firmly believe that there is a unique kind of beauty in crafting and architecting the backend logic of systems. 
+             It's this passion for unraveling the complexity and creating efficient solutions that drives me in my journey as a software engineer."
           </p>
-          <p>
-            Some of the <span>Technologies</span> I have used to build stuff lately:
-          </p>
+          <p><span>Technologies</span> i have used lately are as follows ; </p>
+  
           <ul id="my-tech">
             <li>
               <FontAwesomeIcon icon={faCaretRight} />
